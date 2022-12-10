b9785264c35a6ed0f48ef98378cd57e163fa80eb
Jeffrey Rathgeber Jr <70925870+JeffreyRathgeber@users.noreply.github.com>
2022-12-10 18:43:29 -0500

    Update key_encrypt.cpp

--- key_encrypt.cpp
+++ key_encrypt.cpp
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <stack>
+#include <string>
 #include <iostream>
 #include <fstream>
 #include "tfhe.h"
 #include "tfhe_io.h"
 
-using namespace std;
+// using namespace std;
+
+std::vector<int16_t> string_to_bitarray(const std::string& s) {
+	int n = s.size();
+	std::vector<int16_t> binarystr;
+	std::stack<int16_t> reverse;
+	for (int i = n - 1; i >= 0; i--) {
+		if ((n - i) % 2 == 1) {
+			reverse.push((int16_t)s[i]);	//Every other character, push onto the stack
+		}
+		else {
+			int16_t temp = reverse.top();
+			std::cout << ((int16_t)s[i] << 8) << " ";
+			reverse.pop();                              //Every other character, add the character shifted 1 char size (8bits)
+			reverse.push(((int16_t)s[i] << 8) + (int16_t)temp);    //left to the existing value on the stack
+		}
+
+	}
+	while (!reverse.empty()) {
+		binarystr.push_back(reverse.top());	//Reverse the order
+		reverse.pop();
+	}
+	return binarystr;
+}
 
 // generate cloud keys and put them in appropriate files, will use same key for every 16 bits to encrypt
 void generate_keys(const int minimum_lambda) {
@@ -54,11 +80,18 @@ void export_ciphertext(LweSample* ciphertext, const TFheGateBootstrappingParamet
 
 
 int main() {
+
+    /*
+    TEST STRING INPUT S
+    */
+    const std::string s = "This is my test string.";
+
+
     const int minimum_lambda = 110;
     generate_keys(minimum_lambda);
-    int bits [] = {10, 20, 30};
-    int buffer = sizeof(bits);
-    int arr_index = 0;
+
+    std::vector<int16_t> bitarray = string_to_bitarray(s);
+    int buffer = sizeof(bitarray);
 
     FILE* secret_key = fopen("secret.key","rb");
     TFheGateBootstrappingSecretKeySet* key = new_tfheGateBootstrappingSecretKeySet_fromFile(secret_key);
@@ -71,7 +104,7 @@ int main() {
     const TFheGateBootstrappingParameterSet* params = key->params;
 
     for (int i=0; i<buffer; i++) {
-        export_ciphertext(encrypt16(int16_t(bits[arr_index]), params, key), params);
+        export_ciphertext(encrypt16(bitarray[i], params, key), params);
     }
 
     // LweSample* cipher = new_gate_bootstrapping_ciphertext_array(16, key->params);
