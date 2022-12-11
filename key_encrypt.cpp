d2b1646003b0a26d981f14b59374c2f4ac88ed44
Jeffrey Rathgeber Jr <70925870+JeffreyRathgeber@users.noreply.github.com>
2022-12-10 22:24:01 -0500

    Update key_encrypt.cpp

--- key_encrypt.cpp
+++ key_encrypt.cpp
@@ -10,6 +10,10 @@
 
 // using namespace std;
 
+/*
+INPUT - encrypted answer + secret key
+OUTPUT - int final answer
+*/
 int16_t decrypt(LweSample* answer, TFheGateBootstrappingSecretKeySet* key) {
     int16_t int_answer = 0;
     for (int i=0; i<16; i++) {
@@ -19,6 +23,10 @@ int16_t decrypt(LweSample* answer, TFheGateBootstrappingSecretKeySet* key) {
     return int_answer;
 }
 
+/*
+INPUT - String 
+OUTPUT - Array of 16-bit integers + 2 charracters per int
+*/
 std::vector<int16_t> string_to_bitarray(const std::string& s) {
 	int n = s.size();
 	std::vector<int16_t> binarystr;
@@ -42,7 +50,9 @@ std::vector<int16_t> string_to_bitarray(const std::string& s) {
 	return binarystr;
 }
 
-// generate cloud keys and put them in appropriate files, will use same key for every 16 bits to encrypt
+/*
+generate secret & cloud keys with min lambda and put them in appropriate files
+*/
 void generate_keys(const int minimum_lambda) {
     TFheGateBootstrappingParameterSet* params = new_default_gate_bootstrapping_parameters(minimum_lambda);
 
@@ -67,13 +77,18 @@ void generate_keys(const int minimum_lambda) {
 
 }
 
-// creates empty ciphertext array to be updated by encrypt16()
+/*
+creates empty ciphertext array using parameters from key
+*/
 LweSample* init_ciphertext_array(const TFheGateBootstrappingParameterSet* params) {
     LweSample* initCipher = new_gate_bootstrapping_ciphertext_array(16, params);
     return initCipher;
 }
 
-//encrypt the 16 bits of input
+/*
+INPUT - 16 plaintext bits + ciphertext LweSample to write encrypted data into + secret key
+OUTPUT - 16 bits encrypted data as LweSample
+*/
 LweSample* encrypt16(int16_t plaintext, LweSample* ciphertext, const TFheGateBootstrappingSecretKeySet* key) {
     for (int i=0; i<16; i++) {
         bootsSymEncrypt(&ciphertext[i], (plaintext>>i)&1, key);
@@ -81,6 +96,10 @@ LweSample* encrypt16(int16_t plaintext, LweSample* ciphertext, const TFheGateBoo
     return ciphertext;
 }
 
+/*
+INPUT - 16 ciphertext bits + parameters from key
+Writes encrypted bits to cloud.data file
+*/
 void export_ciphertext(LweSample* ciphertext, const TFheGateBootstrappingParameterSet* params) {
     FILE* cloud_data = fopen("cloud.data","wb");
     for (int i=0; i<16; i++) {
@@ -98,31 +117,37 @@ int main() {
     const std::string s = "This is my test string.";
     const std::string q = "string";
 
-
+    //generates & stores keys
     const int minimum_lambda = 110;
     generate_keys(minimum_lambda);
 
+    //saves secret key to variable 'key'
     FILE* secret_key = fopen("secret.key","rb");
     TFheGateBootstrappingSecretKeySet* key = new_tfheGateBootstrappingSecretKeySet_fromFile(secret_key);
     fclose(secret_key);
 
+    //saves cloud key to variable 'bk'
     FILE* cloud_key = fopen("cloud.key","rb");
     TFheGateBootstrappingCloudKeySet* bk = new_tfheGateBootstrappingCloudKeySet_fromFile(cloud_key);
     fclose(cloud_key);
 
+    //initializes a LweSample object for each of query string and plaintext, to copy from for each encryption iteration
     const TFheGateBootstrappingParameterSet* params = key->params;
     LweSample* initial_ciphertext = init_ciphertext_array(params);
     LweSample* initial_query = init_ciphertext_array(params);
 
+    //converts plaintext to array of 16-bit integers
     std::vector<int16_t> bitarrayS = string_to_bitarray(s);
     int bufferS = sizeof(bitarrayS);
+    //encrypts array one element at a time & stores into cloud.data
     for (int i=0; i<bufferS; i++) {
         export_ciphertext(encrypt16(bitarrayS[i], initial_ciphertext, key), params);
     }
 
-    //encrypts and stores query into query.data
+    //converts query string to array of 16-bit integers
     std::vector<int16_t> bitarrayQ = string_to_bitarray(q);
     int bufferQ = sizeof(bitarrayQ);
+    //encrypts array one element at a time & stores into query.data
     FILE* query_data = fopen("query.data","wb");
     for (int i=0; i<bufferQ; i++) {
         for (int i=0; i<16; i++) {
@@ -133,14 +158,14 @@ int main() {
     
     /*
     STILL TO INTEGRATE:
-        - read LweSamples from query.data and cloud.data
-        - feed LweSamples to comparison algo
+        - read encrypted LweSamples from query.data and cloud.data
+        - feed LweSamples to comparison algo, receive integer answer
         - make integer output answer into 16 bit fixed width integer
         - encrypt and write into answer.data
     */
 
 
-    //decrypt answer.data file
+    //decrypt answer.data file & save final answer into variable 'final answer'
     LweSample* answer = new_gate_bootstrapping_ciphertext_array(16, params);
     FILE* answer_data = fopen("answer.data","rb");
     for (int i=0; i<16; i++) {
